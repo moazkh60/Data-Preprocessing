f990feb195f10539545686120d1165430a7e06d2
Muhammad Moaz Khan <moazkh60@gmail.com>
2019-08-04 20:53:14 +0500

    Encode categorical data
    Encode categorical data

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -15,3 +15,12 @@ y = dataset.iloc[:, 3].values
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
 X[:,1:3] = imputer.fit_transform(X[:, 1:3])
+
+# Taking care of categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelEncoder_x = LabelEncoder()
+X[:,0] = labelEncoder_x.fit_transform(X[:,0])
+oneHotEncoder = OneHotEncoder(categorical_features = [0])
+X = oneHotEncoder.fit_transform(X).toarray()
+labelEncoder_y = LabelEncoder()
+y = labelEncoder_y.fit_transform(y)
